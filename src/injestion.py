f01448283ec7ab9a2dddb08ad61ab946c7f093f7
Alireza Goudarzi <131460345+Alishark14@users.noreply.github.com>
2026-03-13 13:10:01 +0100

    Update injestion.py

--- src/injestion.py
+++ src/injestion.py
@@ -1,12 +1,32 @@
-from pinecone import Pinecone
+import os
+from pinecone import Pinecone, ServerlessSpec
 from langchain_pinecone import PineconeEmbeddings, PineconeVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-def initialize_index(policy_text, api_key):
-    embeddings = PineconeEmbeddings(model="llama-text-embed-v2", pinecone_api_key=api_key)
+def ingest_policy_data(policy_text: str, index_name: str):
+    # Setup Embeddings (NVIDIA Inference)
+    embeddings = PineconeEmbeddings(
+        model="llama-text-embed-v2",
+        pinecone_api_key=os.environ["PINECONE_API_KEY"]
+    )
+
+    # Chunking
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     docs = splitter.create_documents([policy_text])
     
+    # Pinecone setup
+    pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+    if index_name not in [i["name"] for i in pc.list_indexes()]:
+        pc.create_index(
+            name=index_name,
+            dimension=768,
+            metric="cosine",
+            spec=ServerlessSpec(cloud="aws", region="us-east-1")
+        )
+
+    # Vector store instantiation
     return PineconeVectorStore.from_documents(
-        docs, embeddings, index_name="rag-project"
+        documents=docs,
+        embedding=embeddings,
+        index_name=index_name
     )
